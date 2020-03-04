859640682ee86663b75526d80dcd589d216c5169
gg <77777@qq.com>
2020-03-04 15:11:19 +0800

    Store _ CustomerController - 重置新的密码

--- jcart-store/src/main/java/com/gy/jcartstore/controller/CustomerController.java
+++ jcart-store/src/main/java/com/gy/jcartstore/controller/CustomerController.java
@@ -1,7 +1,10 @@
 package com.gy.jcartstore.controller;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.gy.jcartstore.constant.ClientExceptionConstant;
 import com.gy.jcartstore.dto.in.*;
 import com.gy.jcartstore.dto.out.*;
+import com.gy.jcartstore.exception.ClientException;
 import com.gy.jcartstore.po.Customer;
 import com.gy.jcartstore.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,16 +56,29 @@ public class CustomerController {
         customerService.update(customer);
 
     }
-    //传入重置的密码
+
+    //重置新的密码
+    @PostMapping("/changePwd")
+    public void changePwd(@RequestBody CustomerChangePwdInDTO customerChangePwdInDTO,
+                          @RequestAttribute Integer customerId) throws ClientException {
+        Customer customer = customerService.getById(customerId);
+        String encPwdDB = customer.getEncryptedPassword();
+        BCrypt.Result result = BCrypt.verifyer().verify(customerChangePwdInDTO.getOriginPwd().toCharArray(), encPwdDB);
+
+        if (result.verified) {
+            String newPwd = customerChangePwdInDTO.getNewPwd();
+            String bcryptHashString = BCrypt.withDefaults().hashToString(12, newPwd.toCharArray());
+            customer.setEncryptedPassword(bcryptHashString);
+            customerService.update(customer);
+        }else {
+            throw new ClientException(ClientExceptionConstant.CUSTOMER_PASSWORD_INVALID_ERRCODE, ClientExceptionConstant.CUSTOMER_PASSWORD_INVALID_ERRMSG);
+        }
+
+    }
+
     @GetMapping("/getPwdRestCode")
     public String getPwdRest(@RequestParam String email){
         return null;
-    }
-
-    //重置新的密码
-    @PostMapping("/restPwd")
-    public void restPwd(@RequestBody CustomerRestPwdInDTO customerRestPwdInDTO){
-
     }
 
     @GetMapping("/getById")
