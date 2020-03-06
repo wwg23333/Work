77377bfcc91400c14cb89465b7a9286bf74ccb44
gg <77777@qq.com>
2020-03-06 16:55:49 +0800

    Admin - returnService

---
+++ jcart-administration/src/main/java/com/gy/jcartadministration/service/ReturnService.java
@@ -0,0 +1,8 @@
+package com.gy.jcartadministration.service;
+
+import com.gy.jcartadministration.po.Return;
+
+public interface ReturnService {
+    Return getById(Integer returnId);
+
+}
---
+++ jcart-administration/src/main/java/com/gy/jcartadministration/service/impl/ReturnServiceImpl.java
@@ -0,0 +1,8 @@
+package com.gy.jcartadministration.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReturnServiceImpl {
+
+}
