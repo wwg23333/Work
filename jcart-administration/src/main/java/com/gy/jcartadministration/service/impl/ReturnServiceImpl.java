23bbf9b59ea758eddd2bd7ef0e432664217b2b91
gg <77777@qq.com>
2020-03-06 17:04:33 +0800

    admin_ReturnController Write

--- jcart-administration/src/main/java/com/gy/jcartadministration/service/impl/ReturnServiceImpl.java
+++ jcart-administration/src/main/java/com/gy/jcartadministration/service/impl/ReturnServiceImpl.java
@@ -1,8 +1,21 @@
 package com.gy.jcartadministration.service.impl;
 
+import com.gy.jcartadministration.dao.ReturnMapper;
+import com.gy.jcartadministration.po.Return;
+import com.gy.jcartadministration.service.ReturnService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ReturnServiceImpl {
+public class ReturnServiceImpl implements ReturnService {
+
+    @Autowired
+    private ReturnMapper returnMapper;
+
+    @Override
+    public Return getById(Integer returnId) {
+        Return re = returnMapper.selectByPrimaryKey(returnId);
+        return re;
+    }
 
 }
