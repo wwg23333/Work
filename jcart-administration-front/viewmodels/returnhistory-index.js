b76be2bfdd5eb79030e99f548c890734f13fba92
gg <77777@qq.com>
2020-03-10 16:40:48 +0800

    Admin returnHistory _ indexHtml

--- jcart-administration-front/viewmodels/returnhistory-index.js
+++ jcart-administration-front/viewmodels/returnhistory-index.js
@@ -1,4 +1,43 @@
 var app = new Vue({
     el: '#app',
-    data: {}
+    data: {
+        returnId: '',
+        returnHistories: [],
+        selectedReturnStatus: '',
+        returnStatuses: [
+            { value: 1, label: '待取货' },
+            { value: 2, label: '正在处理' },
+            { value: 3, label: '已完成' }
+        ],
+        customerNotified: false,
+        comment: ''
+    },
+    mounted() {
+        console.log('view mounted');
+
+        var url = new URL(location.href);
+        this.returnId = url.searchParams.get("returnId");
+        if (!this.returnId) {
+            alert('returnId is null');
+            return;
+        }
+
+        this.getHistoryByReturnId();
+    },
+    methods: {
+        getHistoryByReturnId() {
+            axios.get('/returnhistory/getListByReturnId', {
+                params: {
+                    returnId: this.returnId
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    app.returnHistories = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
 })
