3e3070017dba2f9006b406f049b68e91fcd7717e
gg <77777@qq.com>
2020-03-10 16:20:18 +0800

    Admin - ReturnHistoryController _ getListByReturnId and a little index Html

--- jcart-administration/src/main/java/com/gy/jcartadministration/controller/ReturnHistoryController.java
+++ jcart-administration/src/main/java/com/gy/jcartadministration/controller/ReturnHistoryController.java
@@ -1,17 +1,37 @@
 package com.gy.jcartadministration.controller;
 import com.gy.jcartadministration.dto.in.returnn.ReturnHistoryCreateInDTO;
 import com.gy.jcartadministration.dto.out.returnn.ReturnHistoryListOutDTO;
+import com.gy.jcartadministration.po.ReturnHistory;
+import com.gy.jcartadministration.service.ReturnHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/returnhistory")
+@CrossOrigin
 public class ReturnHistoryController {
+
+    @Autowired
+    private ReturnHistoryService returnHistoryService;
 
     @GetMapping("/getListByReturnId")
     public List<ReturnHistoryListOutDTO> getListByReturnId(@RequestParam Integer returnId){
-        return null;
+        List<ReturnHistory> returnHistories = returnHistoryService.getListByReturnId(returnId);
+
+        List<ReturnHistoryListOutDTO> returnHistoryListOutDTOS = returnHistories.stream().map(returnHistory -> {
+            ReturnHistoryListOutDTO returnHistoryListOutDTO = new ReturnHistoryListOutDTO();
+            returnHistoryListOutDTO.setReturnHistoryId(returnHistory.getReturnHistoryId());
+            returnHistoryListOutDTO.setTimestamp(returnHistory.getTime().getTime());
+            returnHistoryListOutDTO.setReturnStatus(returnHistory.getReturnStatus());
+            returnHistoryListOutDTO.setComment(returnHistory.getComment());
+            returnHistoryListOutDTO.setCustomerNotified(returnHistory.getCustomerNotified());
+            return returnHistoryListOutDTO;
+        }).collect(Collectors.toList());
+
+        return returnHistoryListOutDTOS;
     }
 
     @PostMapping("/create")
