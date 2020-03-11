439666046f83866a5f97f9e443537608b246361b
gg <77777@qq.com>
2020-03-11 15:48:48 +0800

    Store - OrderController - getList and All order-index

--- jcart-store-front/viewmodels/order-index.js
+++ jcart-store-front/viewmodels/order-index.js
@@ -16,5 +16,27 @@ var app = new Vue({
     },
     mounted() {
         console.log('view mounted');
+        this.getMyOrders();
+    },
+    methods: {
+        handlePageChange(val) {
+            console.log('page changed', val);
+            this.pageNum = val;
+            this.getMyOrders();
+        },
+        getMyOrders() {
+            axios.get('/order/getList', {
+                params: {
+                    pageNum: this.pageNum
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    app.pageInfo = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
     }
 })
