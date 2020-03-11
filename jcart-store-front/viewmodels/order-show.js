b49ad92376003e739f18e2567481b60adc3c44d5
gg <77777@qq.com>
2020-03-11 16:51:38 +0800

    Store - order-show Html

--- jcart-store-front/viewmodels/order-show.js
+++ jcart-store-front/viewmodels/order-show.js
@@ -51,7 +51,31 @@ var app = new Vue({
     },
     methods: {
         getOrderById() {
-           
+            axios.get('/order/getById', {
+                params: {
+                    orderId: this.orderId
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    var orderDTO = response.data;
+                    app.status = orderDTO.status;
+                    app.totalPrice = orderDTO.totalPrice;
+                    app.rewordPoints = orderDTO.rewordPoints;
+                    app.createTimestamp = orderDTO.createTimestamp;
+                    app.updateTimestamp = orderDTO.updateTimestamp;
+                    app.shipMethod = orderDTO.shipMethod;
+                    app.shipAddress = orderDTO.shipAddress;
+                    app.shipPrice = orderDTO.shipPrice;
+                    app.payMethod = orderDTO.payMethod;
+                    app.invoiceAddress = orderDTO.invoiceAddress;
+                    app.invoicePrice = orderDTO.invoicePrice;
+                    app.comment = orderDTO.comment;
+                    app.orderProducts = orderDTO.orderProducts;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         }
     }
 })
