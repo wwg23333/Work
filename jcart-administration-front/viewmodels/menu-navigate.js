821c52e98dfd20560817504af874b1a043812fd0
gg <77777@qq.com>
2020-03-13 16:40:41 +0800

    Admin - route and menu

---
+++ jcart-administration-front/viewmodels/menu-navigate.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    router: router,
+    el: '#app',
+    data: {
+        subMenus: [
+            {
+                name: '',
+                index: '',
+                icon: '',
+                menuItems: []
+            }
+        ]
+    },
+    methods: {
+    }
+})
