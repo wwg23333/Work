4cdeb3149bf3c765fab11b5e3599d1564a962f8e
gg <77777@qq.com>
2020-03-16 17:49:45 +0800

    Admin adminMapperTest

--- jcart-administration/src/test/java/com/gy/jcartadministration/dao/AdminDaoTest.java
+++ jcart-administration/src/test/java/com/gy/jcartadministration/dao/AdminDaoTest.java
@@ -5,6 +5,8 @@ import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AdminDaoTest {
@@ -13,6 +15,11 @@ public class AdminDaoTest {
 
     @Test
     void selectByUsername() {
-
+        String username = "admin";
+        AdministratorMapper administrator = administratorMapper.selectByUsername(username);
+        assertNotNull(administrator);
+        username = "admin110";
+        administrator = administratorMapper.selectByUsername(username);
+        assertNull(administrator);
     }
 }
---
+++ jcart-administration/src/test/java/com/gy/jcartadministration/service/impl/AdminServiceImplTest.java
@@ -0,0 +1,17 @@
+package com.gy.jcartadministration.service.impl;
+
+import com.gy.jcartadministration.service.AdminService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AdminServiceImplTest {
+    @Autowired
+    private AdminService adminService;
+
+    @Test
+    void getByUsername() {
+
+    }
+}
